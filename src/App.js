ec01e632623e3280ba925611ffc87224bf5b7440
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-06-13 08:12:13 +0900

    FEAT :: (#03) 상세페이지 - Router

--- src/App.js
+++ src/App.js
@@ -4,41 +4,52 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {useState} from "react";
 import data from "./data";
+import {Route, Routes, Link, useNavigate, Outlet} from "react-router-dom";
+import Detail from "./pages/Detail";
 
 function App() {
 
     let[shoes] = useState(data);
 
+    let navigate = useNavigate();
+
   return (
     <div className="App">
+
+
         <Navbar bg="light" expand="lg">
             <Container>
                 <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Cart</Nav.Link>
+                        <Link to={'/'}>홈</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Link to={'detail'}>상세페이지</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
 
-        <div className={"main-bg"}></div>
 
-        <div className={"container"}>
-            <div className={'row'}>
-                {/*<Card shoes = {shoes[1]} i = {2}></Card>*/}
-                {/*<Card shoes = {shoes[2]} i = {3}></Card>*/}
-                {
-                    shoes.map((a, i) => {
-                        return (
-                            <Card shoes = {shoes[i]} i = {i}></Card>
-                        )
-                    })
-                }
-            </div>
-        </div>
+        <Routes>
+            <Route path={'/'} element={
+                <>
+                    <div className={"main-bg"}></div>
+                    <div className={"container"}>
+                        <div className={'row'}>
+                            {
+                                shoes.map((a, i) => {
+                                    return (
+                                        <Card shoes={shoes[i]} i={i}></Card>
+                                    )
+                                })}
+                        </div>
+                    </div>
+                </>
+            } />
+
+                    <Route path={'/detail'} element={<Detail/>}></Route>
+                </Routes>
     </div>
   );
 }
