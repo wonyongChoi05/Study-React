ed3d902d8bd4b276e69777613d977e62020fa49d
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-06-12 17:40:47 +0900

    FEAT :: (#01) 홈화면 이미지 추가 - css 부트스트랩 사용

--- src/App.js
+++ src/App.js
@@ -15,31 +15,33 @@ function App() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#link">Cart</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
 
-        <Button variant="primary">Primary</Button>{' '}
-        <Button variant="secondary">Secondary</Button>{' '}
-        <Button variant="success">Success</Button>{' '}
-        <Button variant="warning">Warning</Button>{' '}
-        <Button variant="danger">Danger</Button>{' '}
-        <Button variant="info">Info</Button>{' '}
-        <Button variant="light">Light</Button>{' '}
-        <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+        <div className={"main-bg"}></div>
+
+        <div className={"container"}>
+            <div className={'row'}>
+                <div className={'col-md-4'}>
+                    <img src={"https://codingapple1.github.io/shop/shoes1.jpg"} width={'80%'} alt={"https://codingapple1.github.io/shop/shoes1.jpg"}/>
+                    <h4>상품명</h4>
+                    <p>상품설명</p>
+                </div>
+                <div className={'col-md-4'}>
+                    <img src={"https://codingapple1.github.io/shop/shoes2.jpg"} width={'80%'} alt={"https://codingapple1.github.io/shop/shoes1.jpg"}/>
+                    <h4>상품명</h4>
+                    <p>상품설명</p>
+                </div>
+                <div className={'col-md-4'}>
+                    <img src={"https://codingapple1.github.io/shop/shoes3.jpg"} width={'80%'} alt={"https://codingapple1.github.io/shop/shoes1.jpg"}/>
+                    <h4>상품명</h4>
+                    <p>상품설명</p>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
